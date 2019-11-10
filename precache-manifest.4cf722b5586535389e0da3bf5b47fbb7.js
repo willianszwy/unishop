a61c9750e50a627523ca11e69d2d0c315f54edb4
willians a oliveira <willianszwy@gmail.com>
2019-11-10 16:22:44 -0400

    Updates

---
+++ precache-manifest.4cf722b5586535389e0da3bf5b47fbb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d513c4bbc9200b1539b9e3d20b612c24",
+    "url": "/unishop/index.html"
+  },
+  {
+    "revision": "d0f30811d61b5dd9f967",
+    "url": "/unishop/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d0f30811d61b5dd9f967",
+    "url": "/unishop/static/js/2.d75def61.chunk.js"
+  },
+  {
+    "revision": "81a76f34deb95d83c3bd",
+    "url": "/unishop/static/js/main.aa0a2ffd.chunk.js"
+  },
+  {
+    "revision": "d02dff361e03661932b0",
+    "url": "/unishop/static/js/runtime-main.db5d655c.js"
+  },
+  {
+    "revision": "01c074288e3224a86b50beb305418eb0",
+    "url": "/unishop/static/media/logo.01c07428.svg"
+  }
+]);
